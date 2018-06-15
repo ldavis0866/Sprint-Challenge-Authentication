390509090bf1e80adc1913e34d3f17a9b09de7e9
MacBook <LonnieDavis@MacBookPros-MacBook-Pro.local>
2018-06-15 10:44:34 -0500

    Checkpassword method
    checkpassword method complete api/models/userModels.js

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -37,6 +37,7 @@ UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
+  return bcrypt.compare(plainTextPW, this.password);
 };
 
 module.exports = mongoose.model('User', UserSchema);
