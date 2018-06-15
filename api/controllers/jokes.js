ee7d05dc55b4c2e8a8b6009a75d71c75fb38287e
MacBook <LonnieDavis@MacBookPros-MacBook-Pro.local>
2018-06-15 12:46:20 -0500

    Jokes Endpoint
    Jokes Endpoint complete with accessing token returned from user login and
    use in a GET request that retruned access to jokes data--Sprint Challenge complete

--- api/controllers/jokes.js
+++ api/controllers/jokes.js
@@ -1,10 +1,16 @@
 const fetch = require('node-fetch');
+const jwt = require('jsonwebtoken');
+
+
+
 
 const getAllJokes = (req, res) => {
+  const decoded = req.headers.authorization;
   if (req.decoded) {
     fetch(
       'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
     )
+
       .then(p => p.json())
       .then(jokes => res.json(jokes))
       .catch(err => res.status(500).json({ error: 'Error Fetching Jokes' }));
@@ -12,6 +18,22 @@ const getAllJokes = (req, res) => {
     return res.status(422).json({ error: `Can't get these jokes!` });
   }
 };
+
+/*function authenticate(req, res, next) {
+  const token = req.headers.authorization;
+  if(token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if(err) {
+        res.status(401).json({ message: 'you shall not pass! not decoded'
+       });
+      }
+      next();
+    });
+  } else {
+    res.status(401).json({ message: 'you shall not pass! no token' });
+  }
+}*/
+
 
 module.exports = {
   getAllJokes
